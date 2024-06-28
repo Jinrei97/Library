19a4936497b07fd5be4576beff2f4810391d85b4
Jinrei97 <121897304+Jinrei97@users.noreply.github.com>
2024-06-28 20:23:05 +0200

    Added ability to add new books to the library.
    The "add new book" button now creates a form that
    let's you enter the properties to create a "book"
    object and let's you add a book with those
    properties to the library array.

--- script.js
+++ script.js
@@ -20,15 +20,12 @@ function resetTable(table) {
         table.removeChild(table.lastChild);
     }
 }
-
 function showOnTable() {
     const table = document.querySelector("table");
     resetTable(table);
     for (let book of library) {
-        console.log(book)
         let row = document.createElement("tr");
         for (let property of Object.values(book)) {
-            console.log(property);
             let td = document.createElement("td");
             td.textContent = property;
             row.appendChild(td);
@@ -37,3 +34,65 @@ function showOnTable() {
     }
 }
 showOnTable();
+
+
+function preventDefault(form) {
+    form.addEventListener("submit", e => {
+        e.preventDefault();
+    });
+};
+
+const add = document.querySelector(".add");
+add.addEventListener("click", e => {
+    const form = document.createElement("form");
+
+    function createInput(type, id, placeholder="", label_text, value="") {
+        let div = document.createElement("div");
+        div.className = "container";        
+        let label = document.createElement("label");
+        let input = document.createElement("input");
+        label.textContent = label_text;
+        label.setAttribute("for", id);
+        input.setAttribute("type", type);
+        input.setAttribute("id", id);
+        input.setAttribute("name", id);
+        input.setAttribute("placeholder", placeholder);
+        if (type === "checkbox") {
+            input.value = "no";
+            input.addEventListener("change", e => {
+                input.value = (input.value === "no") ? "yes" : "no";
+            });
+        };
+
+        div.appendChild(label);     
+        div.appendChild(input);
+        form.appendChild(div);
+    }
+
+    createInput("text", "book_name", "", "Title:");
+    createInput("text", "author", "", "Author:");
+    createInput("number", "book_length", "", "Number of Pages:");
+    createInput("checkbox", "read", "", "Already read?");
+
+    let submit = document.createElement("button");
+    submit.addEventListener("click", e => {
+        let inputs = document.querySelectorAll("input");
+        let properties = {};
+        for (let prop of inputs) {
+            properties[prop.getAttribute("id")] = prop.value;
+        }
+        console.log(properties);
+        let book = new Book(properties["book_name"],
+            properties["author"],
+            properties["book_length"],
+            properties["read"]
+        );
+        addBookToLibrary(book);
+    });
+    submit.textContent = "Add book to library";
+    form.appendChild(submit);
+
+    document.querySelector("body").appendChild(form);
+
+    preventDefault(form);
+});
